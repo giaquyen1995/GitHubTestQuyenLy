a9d4cc41b4dcbefcd1063fc464cf54fe8aeaaadd
vfa-quyenlg <quyenlg@vitalify.asia>
2025-05-10 11:22:08 +0700

    create new project

---
+++ GitHubTestQuyenLy/GitHubTestQuyenLyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestQuyenLyApp.swift
+//  GitHubTestQuyenLy
+//
+//  Created by QuyenLG on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestQuyenLyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
