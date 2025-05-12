da35ea2c0c1ea7b28b812a9c8be896e032633486
vfa-quyenlg <quyenlg@vitalify.asia>
2025-05-12 23:38:48 +0700

    update test case for user list

--- Modules/Presentation/Tests/PresentationTests/UsersListViewModelTests.swift
+++ Modules/Presentation/Tests/PresentationTests/UsersListViewModelTests.swift
@@ -209,4 +209,151 @@ final class UsersListViewModelTests: XCTestCase {
         XCTAssertEqual(sut.currentPage, 1)
         XCTAssertTrue(sut.hasLoadMore)
     }
+    
+    
+    func test_isLastItem_whenTrue() {
+        let mockUser1 = MockFactory.createMockUser(login: "user1", id: 1)
+        let mockUser2 = MockFactory.createMockUser(login: "user2", id: 2)
+        let mockUsers = [mockUser1, mockUser2]
+        
+        mockUseCase.mockUsers = mockUsers
+        
+        let expectation = expectation(description: "Wait for state to update")
+        sut.fetchUsers()
+        
+        sut.$state
+            .dropFirst()
+            .sink { state in
+                if case .loaded(let users) = state, users.count == 2 {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 3)
+        
+        let lastUser = sut.users.last!
+        XCTAssertTrue(sut.isLastItem(lastUser), "Should identify last item correctly")
+    }
+    
+    func test_isLastItem_whenFalse() {
+        let mockUser1 = MockFactory.createMockUser(login: "user1", id: 1)
+        let mockUser2 = MockFactory.createMockUser(login: "user2", id: 2)
+        let mockUsers = [mockUser1, mockUser2]
+        
+        mockUseCase.mockUsers = mockUsers
+        
+        let expectation = expectation(description: "Wait for state to update")
+        sut.fetchUsers()
+        
+        sut.$state
+            .dropFirst()
+            .sink { state in
+                if case .loaded(let users) = state, users.count == 2 {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 3)
+        
+        let firstUser = sut.users.first!
+        XCTAssertFalse(sut.isLastItem(firstUser), "First item should not be identified as last item")
+    }
+    
+    // MARK: - Load More Tests
+    
+    func test_loadMore_state() {
+        let initialUsers = [MockFactory.createMockUser(login: "user1", id: 1)]
+        let additionalUsers = [MockFactory.createMockUser(login: "user2", id: 2)]
+        
+        mockUseCase.mockUsers = initialUsers
+        sut.fetchUsers()
+        
+        let initialLoadExpectation = expectation(description: "Initial load")
+        sut.$state
+            .filter { state in
+                if case .loaded(let users) = state, users.count == 1 {
+                    return true
+                }
+                return false
+            }
+            .first()
+            .sink { _ in
+                initialLoadExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [initialLoadExpectation], timeout: 1)
+        
+        mockUseCase.mockUsers = additionalUsers
+        
+        let loadMoreStateExpectation = expectation(description: "LoadMore state detected")
+        sut.$state
+            .dropFirst()
+            .filter { state in
+                if case .loadMore = state {
+                    return true
+                }
+                return false
+            }
+            .first()
+            .sink { _ in
+                loadMoreStateExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        sut.fetchUsers()
+        
+        wait(for: [loadMoreStateExpectation], timeout: 1)
+        XCTAssertTrue(sut.isLoading, "Should be in loading state during loadMore")
+    }
+    
+    func test_loadMore_combinesUsers() {
+        let initialUsers = [MockFactory.createMockUser(login: "user1", id: 1)]
+        let additionalUsers = [MockFactory.createMockUser(login: "user2", id: 2)]
+        
+        mockUseCase.mockUsers = initialUsers
+        sut.fetchUsers()
+        
+        let initialLoadExpectation = expectation(description: "Initial load")
+        sut.$state
+            .filter { state in
+                if case .loaded(let users) = state, users.count == 1 {
+                    return true
+                }
+                return false
+            }
+            .first()
+            .sink { _ in
+                initialLoadExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [initialLoadExpectation], timeout: 1)
+        
+        mockUseCase.mockUsers = additionalUsers
+        
+        let finalLoadExpectation = expectation(description: "Final load with combined users")
+        sut.$state
+            .dropFirst()
+            .filter { state in
+                if case .loaded(let users) = state, users.count == 2 {
+                    return true
+                }
+                return false
+            }
+            .sink { _ in
+                finalLoadExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        sut.fetchUsers()
+        
+        wait(for: [finalLoadExpectation], timeout: 1)
+        XCTAssertEqual(sut.users.count, 2, "Should have combined the users")
+        XCTAssertEqual(sut.users[0].id, 1, "Should preserve initial user")
+        XCTAssertEqual(sut.users[1].id, 2, "Should add new user")
+        XCTAssertEqual(sut.currentPage, 2, "Should increment currentPage")
+    }
 }
